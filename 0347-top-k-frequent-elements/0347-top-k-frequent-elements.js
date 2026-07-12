834f32b38cb657f78db5d5a1f4703eb61774441f
Shivam Dhiman <128705117+shivam0786743@users.noreply.github.com>
2026-07-13 04:17:35 +0530

    Time: 13 ms (54.55%), Space: 57.3 MB (94.2%) - LeetHub

---
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.js
@@ -0,0 +1,36 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number[]}
+ */
+var topKFrequent = function(nums, k) { 
+    let map= {};
+    let bucket=[];
+    let result=[];
+    for (let i=0; i<nums.length;i++){
+        if(!map[nums[i]]){
+            map[nums[i]] = 1;
+        }
+        else{
+            map[nums[i]] ++;
+        }
+    }
+    for(let [num,freq] of Object.entries(map)){
+        num = Number(num)
+        if (!bucket[freq]){
+            bucket[freq] = new Set([num])
+        }
+        else{
+            bucket[freq]= bucket[freq].add(num)
+        }
+    }
+    for(let i=bucket.length-1;i>=0;i--){
+        if(bucket[i]) result.push(...bucket[i]);
+        if(result.length === k) break;
+    }
+    return result
+    
+};
+const nums1=[1,1,1,2,2,3], k=2
+
+console.log(topKFrequent (nums1, k))
